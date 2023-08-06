8b618dc15e0d21e2b95c3f063739da45c4c016b6
Pravee004 <gpraveenkrishnan35@gmail.com>
2023-08-06 15:24:09 +0000

    myfirsttcommit

--- Test2.py
+++ Test2.py
@@ -1,10 +1,32 @@
-print("my first program")
 
-
-create class
-two method
-constructor
-destructor
-
-create object for this class
-call these 2 methods
+class myclass:
+    h=10
+    fruits=["mango","orange"]
+    def __init__(self,name,age,address,contact):
+        self.a=name
+        self.b=age
+        self.c=address
+        self.d=contact
+        print("name:",name)
+        print("age:",age)
+        print("address:",address)
+        print("contact:",contact)
+    def data2(self):
+        print("user data")
+        print("name:",self.a)
+    def __del__(self):
+      self.fruits.clear()
+      print("destructor")
+      print("age:",self.b)
+    def data3(self):
+      print("yes")
+obj=myclass("ram",13,"ffgg",8667537)
+obj.data2()
+obj.data3()
+jj=obj.fruits
+jj.insert(0,"graphs")
+jj.append("apple")
+obj.a="tree"
+print("the value h",obj.a)
+print("fruits",jj)
+obj.__del__()
